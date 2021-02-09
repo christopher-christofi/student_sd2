be46646194c83fa5895861b1ae3c22271ab5a85a
christopher-christofi <christopherlchristofi@outlook.com>
2021-02-09 15:30:42 +0000

    First sql for students db

---
+++ students.sql
@@ -0,0 +1,53 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE Modules (
+code VARCHAR (10) PRIMARY KEY,
+name VARCHAR (50) NOT NULL);
+INSERT INTO Modules VALUES('CMP020C101','Software Development 1');
+INSERT INTO Modules VALUES('CMP020C102','Computer Systems');
+INSERT INTO Modules VALUES('CMP020C103','Mathematics for Computer Science');
+INSERT INTO Modules VALUES('CMP020C104','Software Development 2');
+INSERT INTO Modules VALUES('CMP020C105','Computing and Society');
+INSERT INTO Modules VALUES('CMP020C106','Databases');
+CREATE TABLE Programmes (
+id VARCHAR (8) PRIMARY KEY,
+name VARCHAR (50));
+INSERT INTO Programmes VALUES('09UU0001','BSc Computer Science');
+INSERT INTO Programmes VALUES('09UU0002','BEng Software Engineering');
+INSERT INTO Programmes VALUES('09UU0003','BSc Physics');
+CREATE TABLE Programme_Modules (
+programme VARCHAR (8) NOT NULL,
+module VARCHAR (10) NOT NULL,
+FOREIGN KEY (programme) REFERENCES Programmes (id),
+FOREIGN KEY (module) REFERENCES Modules (code));
+INSERT INTO Programme_Modules VALUES('09UU0001','CMP020C101');
+INSERT INTO Programme_Modules VALUES('09UU0001','CMP020C102');
+INSERT INTO Programme_Modules VALUES('09UU0001','CMP020C103');
+INSERT INTO Programme_Modules VALUES('09UU0001','CMP020C104');
+INSERT INTO Programme_Modules VALUES('09UU0001','CMP020C105');
+INSERT INTO Programme_Modules VALUES('09UU0001','CMP020C106');
+INSERT INTO Programme_Modules VALUES('09UU0002','CMP020C101');
+INSERT INTO Programme_Modules VALUES('09UU0002','CMP020C102');
+INSERT INTO Programme_Modules VALUES('09UU0002','CMP020C103');
+INSERT INTO Programme_Modules VALUES('09UU0002','CMP020C104');
+INSERT INTO Programme_Modules VALUES('09UU0002','CMP020C105');
+INSERT INTO Programme_Modules VALUES('09UU0002','CMP020C106');
+CREATE TABLE Student (
+id VARCHAR (8) PRIMARY KEY,
+name VARCHAR (50) NOT NULL);
+INSERT INTO Student VALUES('STU001','Danny Bane');
+INSERT INTO Student VALUES('STU002','Sally Meek');
+INSERT INTO Student VALUES('STU003','Benny Beep');
+INSERT INTO Student VALUES('STU004','Tammy Tibbs');
+INSERT INTO Student VALUES('STU005','Kevin Keeps');
+CREATE TABLE Student_programme (
+student VARCHAR (8) NOT NULL,
+programme VARCHAR (8) NOT NULL,
+FOREIGN KEY (student) REFERENCES Student (id),
+FOREIGN KEY (programme) REFERENCES Programmes (id));
+INSERT INTO Student_programme VALUES('STU001','09UU0001');
+INSERT INTO Student_programme VALUES('STU002','09UU0001');
+INSERT INTO Student_programme VALUES('STU003','09UU0001');
+INSERT INTO Student_programme VALUES('STU004','09UU0002');
+INSERT INTO Student_programme VALUES('STU005','09UU0002');
+COMMIT;
