bc51506347423bcfadbf18cc4819a1211346bc0c
christopher-christofi <christopherlchristofi@outlook.com>
2021-02-09 18:59:43 +0000

    Initial commit

---
+++ express_server.js
@@ -0,0 +1,24 @@
+// Import express.js
+const express = require("express");
+
+// Create express app
+var app = express();
+
+// Create a get for root - /
+app.get("/", function(req, res) {
+    res.send("Hello world!");
+});
+
+// Create a get for /goodbye
+app.get("/goodbye", function(req, res) {
+    res.send("Goodbye world!");
+});
+
+// Create a get for /hello/<name> with name provided by user
+app.get("/hello/:Ella", function(req, res) {
+    // req.params contains any parameters in the request
+    res.send("Hello " + req.params.name);
+});
+
+// Start server on port 3000
+app.listen(3000);
